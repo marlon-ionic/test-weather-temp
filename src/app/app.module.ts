f971e7a80074bed64b2838fb40fafb0b7680ddb2
Ken Sodemann <ken@ionic.io>
2022-09-08 09:48:39 -0500

    use weather

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { CsdemoTemperatureModule } from '@ionic-enterprise/cs-demo-weather-widgets-angular'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CsdemoTemperatureModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
